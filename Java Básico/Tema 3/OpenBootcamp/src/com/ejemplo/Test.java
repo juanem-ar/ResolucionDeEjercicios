2af48cf43ce3b36fd2dcf0c221f040289836cab3
Juan López <juanem@hotmail.com>
2022-07-16 16:41:30 -0300

    resolucion tema 3

---
+++ Java Básico/Tema 3/OpenBootcamp/src/com/ejemplo/Test.java
@@ -0,0 +1,21 @@
+package com.ejemplo;
+/**
+ * 
+ * Clase principal punto de entrada aplicación
+ * @author Juan
+ */
+public class Test {
+ /**
+ *
+ * @param args argumentos a recibir
+ * @since 1.0
+ */
+    public static void main(String[] args) {
+        
+        String[] nombres = {"Pepe","Juan","Antony"};
+        
+        for (String nombre : nombres) {
+            System.out.print(nombre + " ");
+        }
+    }
+}
